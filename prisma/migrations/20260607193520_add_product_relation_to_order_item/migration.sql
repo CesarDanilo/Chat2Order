915a2316fd9bdfdaef598a0dac0680e4146c0964
Cesar <billypalacios616@gmail.com>
2026-06-07 15:39:54 -0400

    feat: create table products

---
+++ prisma/migrations/20260607193520_add_product_relation_to_order_item/migration.sql
@@ -0,0 +1,19 @@
+-- AlterTable
+ALTER TABLE "OrderItem" ADD COLUMN     "productId" TEXT;
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+    "category" TEXT,
+    "available" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE SET NULL ON UPDATE CASCADE;
