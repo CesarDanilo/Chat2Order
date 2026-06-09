3c80785d73f8b2e44d1883f53134757a359926ca
Cesar <billypalacios616@gmail.com>
2026-06-08 21:56:48 -0400

    feat-new-feacture-products

--- src/interfaces/IProductsRepository.ts
+++ src/interfaces/IProductsRepository.ts
@@ -1,16 +1,13 @@
 import { Product } from "@prisma/client";
 
 export interface CreateProducts {
-    id: string;
-    userId: string;
-    name: string;
-    description: string;
-    price: number;
-    category: string;
-    available: boolean;
-    createdAt: Date;
+  userId: string;
+  name: string;
+  description?: string;
+  price: number;
+  category?: string;
+  available?: boolean;
 }
-
 
 export interface IProductRepository {
     create(data: CreateProducts): Promise<Product>;
--- src/repository/PrismaProductsRepository.ts
+++ src/repository/PrismaProductsRepository.ts
@@ -1,27 +1,33 @@
 import { prisma } from "../config/prisma";
+import {
+  IProductRepository,
+  CreateProducts,
+} from "../interfaces/IProductsRepository";
 
-import { IProductRepository, CreateProducts } from "../interfaces/IProductsRepository";
-
-export class PrismaProductsRepository implements IProductRepository{
+export class PrismaProductsRepository
+  implements IProductRepository {
 
   async create(data: CreateProducts) {
     const { userId, ...rest } = data;
 
-    return await prisma.order.create({
+    return await prisma.product.create({
       data: {
         ...rest,
 
         user: {
-          connect: { id: userId },
+          connect: {
+            id: userId,
+          },
         },
-    }});
+      },
+    });
   }
-  
-    async findById(id: string) {
-        return prisma.product.findUnique({
-            where: {
-                id
-            }
-        })
-    }
+
+  async findById(id: string) {
+    return prisma.product.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
 }
